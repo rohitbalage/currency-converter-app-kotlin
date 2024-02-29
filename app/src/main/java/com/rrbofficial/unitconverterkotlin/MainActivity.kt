7f9bbc30126f3ee9c8c20d6a35c1c52ee3543b40
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-29 05:12:58 -0500

    added logic and enhance UI

--- app/src/main/java/com/rrbofficial/unitconverterkotlin/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/unitconverterkotlin/MainActivity.kt
@@ -11,10 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edt : EditText = findViewById(R.id.kilo_edit)
+        val edt : EditText = findViewById(R.id.dollar_edittext)
         val resulttext : TextView = findViewById(R.id.output_text)
         val btn : Button = findViewById(R.id.button)
 
 
+
+        btn.setOnClickListener()
+        {
+            val dollars: Double = edt.text.toString().toDouble()
+            resulttext.setText(""+convertToRuppes(dollars) + "\n Ruppes")
+        }
+
+    }
+
+    fun convertToRuppes(dollars : Double): Double
+    {
+    var ruppes  = dollars * 82
+
+        return  ruppes;
     }
 }
